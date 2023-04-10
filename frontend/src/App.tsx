668f417606b1c3f220974edb71b232a99d6095e2
AndreTheViking <andre.masson@norda.com>
2023-04-10 16:07:25 -0600

    feat: extract browser name from user agent parsing library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
+import UAParser from 'ua-parser-js';
 import { Echo } from './domain/echo';
 
 function App() {
   const [data, setData] = useState<Echo>();
+  const ua = new UAParser();
 
   useEffect(() => {
     fetch('/api')
@@ -12,17 +14,30 @@ function App() {
 
   return (
     <div className="container mx-auto bg-gray-100 rounded-xl shadow border p-8 m-4">
-
       <div>
-        message: <strong>{data?.message}</strong> server timestamp: <strong>{data?.timestamp}</strong>
+        <p>Current browser: <strong>{ua.getBrowser().name}</strong></p>
+        <strong>{data?.message}</strong> server timestamp: <strong>{data?.timestamp}</strong>
       </div>
 
+      <br />
+
       <ul>
-      <li><a className='underline cursor-pointer' href="/api/download/buffer">Example of [icon.png] file download 1 (buffer)</a></li>
-      <li><a className='underline cursor-pointer' href="/api/download/stream">Example of [icon.png] file download 2 (stream)</a></li>
-      <li><a className='underline cursor-pointer' href="/api/download/streamable">Example of [package.json] file download</a></li>
+        <li>
+          <a className="underline cursor-pointer" href="/api/download/buffer">
+            Example of [icon.png] file download 1 (buffer)
+          </a>
+        </li>
+        <li>
+          <a className="underline cursor-pointer" href="/api/download/stream">
+            Example of [icon.png] file download 2 (stream)
+          </a>
+        </li>
+        <li>
+          <a className="underline cursor-pointer" href="/api/download/streamable">
+            Example of [example.json] file download
+          </a>
+        </li>
       </ul>
-      
     </div>
   );
 }
